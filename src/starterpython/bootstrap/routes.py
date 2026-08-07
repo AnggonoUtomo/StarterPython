96e9600d1d8c0dbf57e0df37da149b0b39c3ae54
Gotrok <anggonoutomo@gmail.com>
2026-08-07 09:56:47 +0700

    register users router

--- src/starterpython/bootstrap/routes.py
+++ src/starterpython/bootstrap/routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from starterpython.modules.system.presentation.routes import router as system_router
+from starterpython.modules.users.presentation.routes import router as users_router
 
 api_router = APIRouter()
 api_router.include_router(system_router)
+api_router.include_router(users_router)
